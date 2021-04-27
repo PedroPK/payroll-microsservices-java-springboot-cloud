269f63c09d1a445aa0b25efaf19c34ed6babc0a7
pedropk <pedropk@gmail.com>
2021-04-27 08:03:52 -0300

    Rest Template implemented
    Work entity class was duplicated.
    
    Projects Payroll and Worker are communicating
    
    No Microservices yet

--- hr-payroll/src/main/java/com/udemy/devSuperior/hrPayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/devSuperior/hrPayroll/services/PaymentService.java
@@ -1,17 +1,38 @@
 package com.udemy.devSuperior.hrPayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemy.devSuperior.hrPayroll.entities.Payment;
+import com.udemy.devSuperior.hrPayroll.entities.WorkerDup;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(
 		long 	pWorkerId,
 		int		pDays
 	) {
-		return new Payment("Bob", 200.0, pDays);
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("id", String.valueOf(pWorkerId));
+		
+		WorkerDup worker = restTemplate.getForObject(
+			workerHost + "/workers/{id}", 
+			WorkerDup.class,
+			uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), pDays);
 	}
 
 }
